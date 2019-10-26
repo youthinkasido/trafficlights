a18dcaef19268059d9beb4c423768248d6b5934f
Arshia <youthinkasido@gmail.com>
2019-10-25 21:29:00 -0400

    Refactored with case statements

--- app.js
+++ app.js
@@ -3,38 +3,36 @@ script.src = 'https://code.jquery.com/jquery-3.4.1.min.js';
 script.type = 'text/javascript';
 document.getElementsByTagName('head')[0].appendChild(script);
 
-
 window.addEventListener('load', () => {
     var currentLight = 1;
     $("#green-light").css("background-color", "green")
     let changeLightButton = document.getElementById('change-light-button')
     changeLight(changeLightButton, currentLight)
-
 })
 function changeLight(changeLightButton, currentLight) {
-
     changeLightButton.addEventListener('click', function () {
-        console.log('light that comes in', currentLight)
-        if (currentLight === 1) {
+        var green = $("#green-light"); var yellow = $("#yellow-light"); var red = $("#red-light")
 
-            $("#green-light").css("background-color", "grey")
-            $("#yellow-light").css("background-color", "yellow")
-            $("#red-light").css("background-color", "grey")
+        switch (currentLight) {
+            case 1:
+                green.css("background-color", "grey")
+                yellow.css("background-color", "yellow")
+                red.css("background-color", "grey")
+                break;
+            case 2:
 
-        } else if (currentLight === 2) {
-            $("#green-light").css("background-color", "grey")
-            $("#yellow-light").css("background-color", "grey")
-            $("#red-light").css("background-color", "red")
+                green.css("background-color", "grey")
+                yellow.css("background-color", "grey")
+                red.css("background-color", "red")
+                break;
+            case 3:
 
-        } else if (currentLight === 3) {
-            $("#green-light").css("background-color", "green")
-            $("#yellow-light").css("background-color", "grey")
-            $("#red-light").css("background-color", "grey")
-            currentLight = 1;
-            return
+                green.css("background-color", "green")
+                yellow.css("background-color", "grey")
+                red.css("background-color", "grey")
+                currentLight = 0;
+                break;
         }
         currentLight++
     })
-
-    console.log('light that goes out', currentLight)
 }
